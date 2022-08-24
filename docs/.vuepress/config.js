e60c2dedba25cfe61a217a6a0f054b315ce4142e
teatube <teatube@qq.com>
2022-08-24 09:46:33 +0800

    更改base路径

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,7 @@ const { defaultTheme } = require('@vuepress/theme-default')
 module.exports = {
     title : 'Tetris Online (Study) 使用说明',
     description : '方块茶服。Tetris Online (Japan) 的 俄罗斯方块研究群用 线上研究服。因服主名称含【茶】因此也称作【茶服】',
+    base : '/tos/',
     theme: defaultTheme({
         navbar: [
         // NavbarItem
